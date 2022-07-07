1ed6794fbab5731a2a263141ffcdc475d4c88c93
Apostolos Papas <apostolospapas@Apostoloss-iMac.lan>
2022-07-07 11:45:59 +0300

    Updated Object name in movieConverter function.

--- src/converters/movieConverter.ts
+++ src/converters/movieConverter.ts
@@ -1,12 +1,12 @@
 const PREFIX: string = 'https://image.tmdb.org/t/p/w500'; 
 
-export const movieConverter = (movieTitle: TmdbMovie): Movie => {
+export const movieConverter = (tmdbMovie: TmdbMovie): Movie => {
     return {
-        movieId: movieTitle.id,
-        title: movieTitle.title,
-        releaseDate: movieTitle.release_date,
-        backdropPath: `${PREFIX}/${movieTitle.backdrop_path}`,
-        posterPath: `${PREFIX}/${movieTitle.poster_path}`,
-        voteAverage: movieTitle.vote_average,
+        movieId: tmdbMovie.id,
+        title: tmdbMovie.title,
+        releaseDate: tmdbMovie.release_date,
+        backdropPath: `${PREFIX}/${tmdbMovie.backdrop_path}`,
+        posterPath: `${PREFIX}/${tmdbMovie.poster_path}`,
+        voteAverage: tmdbMovie.vote_average,
     };
 }; 
