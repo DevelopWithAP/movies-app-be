e3b4127eaee1a89edb7499a23f39b2560ecd1cbb
Apostolos Papas <apostolospapas@Apostoloss-iMac.lan>
2022-07-25 17:34:00 +0300

    Added movie route to movie.routes.ts

--- src/controllers/movies.controller.ts
+++ src/controllers/movies.controller.ts
@@ -1,9 +1,9 @@
 import { Request, Response, NextFunction } from 'express';
-import * as MoviesService from '../services/movies.service';
+import * as moviesService from '../services/movies.service';
 
 export const getMovies = async (_req: Request, res: Response, next: NextFunction): Promise<void> => {
   try {
-    res.json(await MoviesService.getMovies());
+    res.json(await moviesService.getMovies());
   } catch (error) {
     next(error);
   }
@@ -11,7 +11,7 @@ export const getMovies = async (_req: Request, res: Response, next: NextFunction
 
 export const getMovie = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   try {
-    res.json(await MoviesService.getMovie(parseInt(req.params.movieId)));
+    res.json(await moviesService.getMovie(parseInt(req.params.movieId)));
   } catch (error) {
     next(error);
   }
