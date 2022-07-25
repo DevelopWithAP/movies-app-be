7af8dc16fd28dd9e09e208779400e059da05fc7e
Apostolos Papas <apostolospapas@Apostoloss-iMac.lan>
2022-07-25 17:21:45 +0300

    Changed variable naming

--- src/controllers/movies.controller.ts
+++ src/controllers/movies.controller.ts
@@ -1,9 +1,9 @@
 import { Request, Response, NextFunction } from 'express';
-import * as MoviesService from '../services/movies.service';
+import * as moviesService from '../services/movies.service';
 
 export const getMovies = async (_req: Request, res: Response, next: NextFunction): Promise<void> =>  {
     try {
-        res.json(await MoviesService.getMovies());
+        res.json(await moviesService.getMovies());
     } catch (error) {
         next(error);
     } 
