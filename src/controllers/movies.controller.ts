4b4a3479dc1b7102909412cfe005d00ea758fa82
Apostolos Papas <apostolospapas@Apostoloss-iMac.lan>
2022-08-11 18:14:18 +0300

    Filter by title and unit testing started.

--- src/controllers/movies.controller.ts
+++ src/controllers/movies.controller.ts
@@ -1,10 +1,18 @@
 import { Request, Response, NextFunction } from 'express';
 import * as moviesService from '../services/movies.service';
+import { validate } from '../validators/title.validator';
 
 export const getMovies = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   try {
     let page: number = Number(req.query.page) || 1;
-    res.json(await moviesService.getMovies(page));
+    let title: string = req.query.title as string;
+
+    if (title && validate(title) && page) {
+      res.json(moviesService.searchMoviesByTitle(title, page));
+    }
+    else if (page) {
+      res.json(moviesService.getMovies(page));
+    }
   } catch (error) {
     next(error);
   }
@@ -17,3 +25,4 @@ export const getMovie = async (req: Request, res: Response, next: NextFunction):
     next(error);
   }
 };
+
--- src/services/movies.service.ts
+++ src/services/movies.service.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { AnyArray } from "mongoose";
 import { movieConverter, convertToMovieDetails } from '../converters/movie.converter';
 
 const API_KEY: string = process.env.API_KEY as string;
@@ -23,7 +24,7 @@ export const getMovies = async (page: number): Promise<Movies> => {
 
     moviesCache[page] = [];
     moviesCache.totalPages = data.total_pages;
- 
+
     let moviesArray: Movie[] = data?.results.map(movieConverter);
     for (let movie of moviesArray) {
       moviesCache[page].push(movie);
@@ -33,18 +34,30 @@ export const getMovies = async (page: number): Promise<Movies> => {
   return {
     page,
     totalPages: moviesCache.totalPages || 0,
-    movies: moviesCache[page], 
+    movies: moviesCache[page],
   };
 
 };
 
 export const getMovie = async (movieId: number): Promise<MovieDetails> => {
   const GET_MOVIE_API_ENDPOINT: string = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}`;
-  if(movieDetailsCache[movieId]) {
+  if (movieDetailsCache[movieId]) {
     return movieDetailsCache[movieId];
-  } 
+  }
   const { data } = await axios.get<TmdbMovieDetails>(GET_MOVIE_API_ENDPOINT);
 
   movieDetailsCache[movieId] = convertToMovieDetails(data);
   return movieDetailsCache[movieId];
+};
+
+export const searchMoviesByTitle = async (title: string, page: number): Promise<Movies>  => {
+  const SEARCH_MOVIES_API_ENDPOINT: string = `https://api.themoviedb.org/3/search/movie?query=${title}&page=${page}&api_key=${API_KEY}`;
+
+  const { data } = await axios.get<TmdbMovies>(SEARCH_MOVIES_API_ENDPOINT);
+
+  return {
+    page: page,
+    totalPages: data?.total_pages,
+    movies: data.results.map(movieConverter),
+  };
 };
