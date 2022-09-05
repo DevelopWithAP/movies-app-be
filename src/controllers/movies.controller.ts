cfc90731b99a821cd640941571beac3e1d5249b4
Apostolos Papas <apostolospapas@Apostoloss-iMac.lan>
2022-09-05 12:33:58 +0300

    Branches synched

--- src/controllers/movies.controller.ts
+++ src/controllers/movies.controller.ts
@@ -1,14 +1,19 @@
 import { Request, Response, NextFunction } from 'express';
 import * as moviesService from '../services/movies.service';
 import { validate } from '../validators/title.validator';
+import { validateGenres } from '../validators/genre.validator';
 
 export const getMovies = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   try {
     const page: number = Number(req.query.page) || 1;
     const title: string = req.query.title as string;
+    const genres: string = req.query.genres as string;
 
     if (title && validate(title)) {
       res.json(await moviesService.searchMoviesByTitle(title, page));
+    }
+    else if (genres && validateGenres(genres)) {
+      res.json(await moviesService.filterMoviesByGenres(page, genres));
     }
     else {
       res.json(await moviesService.getMovies(page));
