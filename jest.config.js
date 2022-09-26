5f66534a8cb46102f2545665dddb52d7b70f3d66
DevelopWithAP <apostolisparga@gmail.com>
2022-09-26 11:39:23 +0300

    new: added tests

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: '@shelf/jest-mongodb',
+  preset: 'ts-jest',
   testEnvironment: 'node',
 };
--- tests/integration/sign-up.test.ts
+++ tests/integration/sign-up.test.ts
@@ -1,28 +1,66 @@
-import { connectToTestMongoDb, disconnectFromTestMongoDb } from './connectToTestDB';
-import { UserModel } from '../../src/models/user';
+import nock from 'nock';
 
+const target_url = 'http://localhost:3001';
 
-describe('Testing User model', () => {
+describe('Testing /sign-up route', () => {
 
-    beforeAll(async () => {
-        await connectToTestMongoDb();
+    it('/sign-up rejects malformed password', async () => {
+        nock(target_url)
+            .post('/sign-up', {
+                name: 'user',
+                email: 'user@mail.com',
+                password: 'letmein'
+            })
+            .replyWithError({
+                'msg': 'Password must contain at least 8 characters; one upper case character, one number and one special character'
+            });
     });
 
-    afterAll(async () => { 
-        await disconnectFromTestMongoDb(); 
-    });
-
-    test('Testing sign-up', async () => {
-        const user = {
-            name: 'User',
-            email: 'user@mail.com',
-            password: 'letMeIn'
+    it('/sign-up rejects malformed name', async () => {
+        const malformedNameInPayload = {
+            name: 'Me',
+            email: 'me@mail.com',
+            password: 'myVerySecurePassword#1'
         };
 
-        const newUser = new UserModel({...user});
-        const result = await newUser.save();
-        
-        expect(result.name).toEqual('User');
+        nock(target_url)
+            .post('/sign-up')
+            .replyWithError({
+                "value": "Me",
+                "msg": "Invalid value",
+                "param": "name",
+                "location": "body"
+            });
     });
 
+    it('/sign-up rejects malformed email', async () => {
+        const malformedNameInPayload = {
+            name: 'Me',
+            email: 'memail.com',
+            password: 'myVerySecurePassword#1'
+        };
+
+        nock(target_url)
+            .post('/sign-up')
+            .replyWithError({
+                "value": "memail.com",
+                "msg": "Invalid value",
+                "param": "email",
+                "location": "body"
+            });
+    });
+
+    it('/sign-up accepts payload', async () => {
+        const correctPayload = {
+            name: 'user',
+            email: 'user@mail.com',
+            password: 'myVerySecurePassword#1'
+        };
+
+        nock(target_url)
+            .post('/sign-up', correctPayload)
+            .reply(200, {
+                access_token: expect.stringMatching(/^(?:[\w-]*\.){2}[\w-]*$/)
+            });
+    })
 });
